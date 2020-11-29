a652b174865a8d8bcf369cc3d0038bcc2fc51994
arnauochoa <arnau.ochoa@gmail.com>
2020-11-29 18:53:43 +0100

    Skog and Handel method partially implemented. Some questions to be solved

---
+++ Estimation_Methods/julierCU.m
@@ -0,0 +1,15 @@
+function [rInt, PEst] = julierCU(rInt, pGNSS, measAcc, tIMU, POld, sigmaAcc, sigmaGNSS)
+% julierCU: This function estimates the position and velocity using the 
+%           method described by Julier and Uhlman 
+%
+% Inputs:   
+%
+% Outputs:  
+
+
+
+
+
+
+
+end
--- Estimation_Methods/skogEKF.m
+++ Estimation_Methods/skogEKF.m
@@ -1,4 +1,4 @@
-function [xNew, PNew] = skogEKF(pIMU, pGNSS, tIMU, rOld, POld, sigmaAcc, sigmaGNSS, measAcc)
+function [rInt, PEst, measAccCorr] = skogEKF(rInt, pGNSS, measAcc, measAccCorr, k, t, tIMU, POld, sigmaAcc, sigmaGNSS)
 % skogEKF:  This function estimates the position, velocity and time delay error using the Skog and Handel
 %           method (augmented error EKF). 
 %
@@ -8,44 +8,55 @@ function [xNew, PNew] = skogEKF(pIMU, pGNSS, tIMU, rOld, POld, sigmaAcc, sigmaGN
 % Outputs:  delta x:      Error state vector estimation
 
 % Initialization
-deltaK = ~isnan(pGNSS);
+F = [1 tIMU 0 0; 0 1 0 0; 0 0 1 0; 0 0 0 1];
+Q = [0 0 0 0; 0 tIMU*sigmaAcc^2 0 0; 0 0 0 0; 0 0 0 0];
 
-F = [1 tIMU 0; 0 1 0; 0 0 1];
-Q = [sigmaAcc 0 0; 0 sigmaAcc 0; 0 0 0];
+% Sensor error compensation
+measAccCorr(k) = measAcc(k) + rInt(3);
 
-% State prediction
+% Interpolation of accelerometer measurement at t-Td
+measAccInt = interp1(t, measAccCorr(1:k), rInt(4));
 
-rEst = F*rOld;
-PEst = F*POld*F' + Q;
+if(isnan(pGNSS)) % No GNSS observations
+    
+    % Strapdown equations
+    rInt(2) = rInt(2) + measAccCorr(k) * tIMU;
+    rInt(1) = rInt(1) + rInt(2) * tIMU;
 
-% State Update
-if(~isnan(pGNSS))
-    H = [1 0];
-    R = [sigmaGNSS];
-    z = pGNSS - H * rEst;
+    % Sensor error update
+    rInt(3) = rInt(3);
+
+    % State prediction
+    PEst = F*POld*F' + Q;
+
+else % GNSS observations
+    
+    H = [1 0 0 -rInt(2)];
+    R = [sigmaGNSS^2];
     K = (POld*H')/(H*POld*H' + R);
-    xEst = K*z;
-    rEst = rEst + xEst;
-    PEst = PEst - K*H*PEst;
+    z = pGNSS - H * rInt;
+    Kp = F*K;
+    d = measAccInt * (1/2) * (rInt(4)^2);
+    expX = pinv(eye(size(F))-F) * (-(1/2) * Kp * measAccInt * POld(4,4));
+    piFactor = (1/4)*(measAccInt^2)*(3*(POld(4,4)^2) - 2*(expX(4)^4));
+    gammaFactor = (POld(4,4)*expX + 2*expX(4)*(POld(1:4,4) - expX(4)*expX))*(measAccInt/2);
+    
+    xEst = Kp*(z - d);
+    rInt = rInt + xEst;
+    PEst = POld + Kp*R*Kp' + Kp*piFactor*Kp' - F*gammaFactor*Kp' - Kp*gammaFactor'*F';
+    
+    % Sensor error compensation
+    measAccCorr = measAccInt + rInt(4);
+    % Strapdown equations updated
+    rInt(2) = rInt(2) + measAccCorr * tIMU;
+    rInt(1) = rInt(1) + rInt(2) * tIMU;
+    % Sensor error update
+    rInt(3) = rInt(3);
+    
+%     % Correction from t-Td to t
+%     F = [1 rInt(3) 0; 0 1 0; 0 0 1];
+%     xEst = F*xEst;
+%     PEst = F*PEst*F' + Q;
 end
-
-
-z = deltaK*(pGNSS - pIMU);
-d = measAcc*(1/2)*(xOld(3)^2);
-H = deltaK*[1 0 -xOld(2)];
-R = [sigmaGNSS];
-K = (H*POld*H' + R)\(POld*H'); 
-Kp = F*K;
-expX = pinv(eye(size(F))-F)*(-(1/2)*Kp*measAcc*POld(3,3));
-piFactor = (1/4)*(measAcc^2)*(3*(POld(3,3)^2) - 2*(expX(3)^4));
-gammaFactor = (POld(3,3)*expX + 2*expX(3)*(POld(1:3,3) - expX(3)*expX))*(measAcc/2);
-%xEst = F*xOld;
-%PEst = F*POLd*F' + Q;
-%K = (H*PEst*H' + R)\(PEst*H');
-%xPred = xOld + K*z;
-%PPred = PEst - K*H*PEst;
-
-xNew = (F - Kp*H)*xOld - Kp*d;
-PNew = F*POld*F' + Kp*R*Kp' + Q + Kp*piFactor*Kp' - F*gammaFactor*Kp' - Kp*gammaFactor'*F';
 
 end
