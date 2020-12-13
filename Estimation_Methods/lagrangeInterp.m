f33ed6139f768e29fccd35e02cacf61192e8dfc8
arnauochoa <arnau.ochoa@gmail.com>
2020-12-13 13:54:50 +0100

    lagrange interpolation implemented, debug of skog still in process

---
+++ Estimation_Methods/lagrangeInterp.m
@@ -0,0 +1,46 @@
+function v = lagrangeInterp(t, y, D, maxN)
+
+%% Find index in t closest to delay D
+[~, closestIndex] = min(abs(D - t.'));
+
+%% Window size computation
+dEnd    = length(t) - closestIndex; % Distance from delay to last time
+dBegin  = closestIndex - 1; % Distance from delay to first time
+
+% Deal with extreme values
+dEnd    = max(dEnd, 1); 
+dBegin  = max(dBegin, 1);
+
+N       = 2 * min(dEnd, dBegin); % Time window - 1
+
+% if N > maxN, N = maxN; end
+
+% Indexes of y inside window
+kWindow = max(closestIndex-(N/2), 1) : min(closestIndex+(N/2), length(t)); 
+
+%% Weights of the FIR
+h   = zeros(1, length(kWindow));
+% Adding an appropiate number of unit delays before the fractional delay FIR filter
+n   = t(kWindow);
+for i = 1:length(kWindow)
+    k = n;
+    k(i) = [];
+    h(i) = prod( (D-k) ./ (n(i)-k) );
+end
+% Flip h so h(0) multiplies last y
+% h = flip(h);
+
+%% Interpolation
+v = sum(dot(y(kWindow), h));
+
+% figure; 
+% plot(n, h, 'o-');
+% title('Weights');
+% xline(D)
+% 
+% figure;
+% plot(t, y, 'o-'); hold on;
+% plot(D, v, 'x');
+% xline(D)
+
+end
--- Estimation_Methods/skogEKF.m
+++ Estimation_Methods/skogEKF.m
@@ -1,4 +1,5 @@
-function [rIMU, PEst, measAccCorr] = skogEKF(rIMU, PEst, pGNSS, measAcc, measAccCorr, k, t, tIMU, sigmaAcc, sigmaGNSS)
+function [rIMUEst, PEst, measAccCorr] = ...
+    skogEKF(rIMU, PEst, pGNSS, measAcc, measAccCorr, k, tspan, tIMU, sigmaAcc, sigmaGNSS)
 % skogEKF:  This function estimates the position, velocity and time delay error using the Skog and Handel
 %           method (augmented error EKF). 
 %
@@ -7,52 +8,68 @@ function [rIMU, PEst, measAccCorr] = skogEKF(rIMU, PEst, pGNSS, measAcc, measAcc
 %
 % Outputs:  delta x:      Error state vector estimation
 
+% --> TODO: define indexes of state vector as variables
+
+rIMUPrev = rIMU(:, k-1);
+
 % Sensor error compensation
-measAccCorr(k) = measAcc(k) + rIMU(3);
+measAccCorr(k) = measAcc(k) + rIMUPrev(3);
 
 % Interpolation of accelerometer measurement at t-Td
-measAccInt = interp1(t, measAccCorr(1:k), t(end) - rIMU(4)); % TODO: check constraint for Td
+measAccInt = lagrangeInterp(tspan(1:k), measAccCorr(1:k), tspan(k) - rIMUPrev(4), 30); % TODO: check constraint for Td
 
 % Initialization
 F = [1 tIMU 0 0; 0 1 tIMU 0; 0 0 1 0; 0 0 0 1];
 Q = [0 0 0 0; 0 (tIMU^2)*sigmaAcc^2 0 0; 0 0 sigmaAcc^2 0; 0 0 0 0];
 
-if(~isnan(pGNSS)) % If GNSS position is available  
-    H = [1 0 0 -rIMU(2)];
+if(~isnan(pGNSS)) % If GNSS position is available
+    %% Estimate GNSS update at k-Td
+    H = [1 0 0 -rIMUPrev(2)];
     R = [sigmaGNSS^2];
     K = (PEst*H')/(H*PEst*H' + R);
-    z = pGNSS - H * rIMU;
+    z = pGNSS - H * rIMUPrev(:);
     Kp = F*K;
-    d = measAccInt * (1/2) * (rIMU(4)^2);
+    d = measAccInt * (1/2) * (rIMUPrev(4)^2);
     expX = pinv(eye(size(F))-F) * (-(1/2) * Kp * measAccInt * PEst(4,4));
     piFactor = (1/4)*(measAccInt^2)*(3*(PEst(4,4)^2) - 2*(expX(4)^4));
     gammaFactor = (PEst(4,4)*expX + 2*expX(4)*(PEst(1:4,4) - expX(4)*expX))*(measAccInt/2);
     
-    % Move rIMU to k-Td
-    Ftd = [1 -rIMU(4) 0 0; 0 1 -rIMU(4) 0; 0 0 1 0; 0 0 0 1];
-    rIMUtd = Ftd * rIMU;
+    %% Move rIMU from k-1 to k-Td
+    rIMUtd = zeros(4, 1);
+    for i=1:4
+        rIMUtd(i) = lagrangeInterp(tspan(1:k-1), rIMU(i, 1:k-1), tspan(k-1) - rIMUPrev(4), 30);
+    end
     
-    % Update rIMU with xEst at k-Td
+    %% Update rIMU with xEst at k-Td
     xEst = Kp*(z - d);
     rIMUtd = rIMUtd + xEst;
     PEst = PEst + Kp*R*Kp' + Kp*piFactor*Kp' - F*gammaFactor*Kp' - Kp*gammaFactor'*F';
     
-    % Move updated rIMU from k-Td to k
-    Ftd = [1 rIMU(4) 0 0; 0 1 rIMU(4) 0; 0 0 1 0; 0 0 0 1];
-    Qtd = [0 0 0 0; 0 rIMU(4)*sigmaAcc^2 0 0; 0 0 0 0; 0 0 0 0];
-    rIMU = Ftd*rIMUtd;
-    PEst = Ftd*PEst*Ftd' + Qtd;
+    %% Move updated rIMU from k-Td to k-1
+    Ftd         = [ 1 rIMUPrev(4) 0 0;              ...
+                    0 1 rIMUPrev(4) 0;              ...
+                    0 0 1 0;                        ...
+                    0 0 0 1];
+                
+    Qtd         = [ 0 0 0 0;                        ...
+                    0 rIMUPrev(4)*sigmaAcc^2 0 0;   ...
+                    0 0 0 0;                        ...
+                    0 0 0 0];
+                
+    rIMUPrev    = Ftd * rIMUtd;
+    PEst        = Ftd*PEst*Ftd' + Qtd;
 end
 
+%% Predict rIMU from k-1 to k
 % Sensor error compensation
-measAccCorr(k) = measAccInt + rIMU(3);
+measAccCorr(k) = measAccInt + rIMUPrev(3);
 % Strapdown equations updated
-rIMU(2) = rIMU(2) + measAccCorr(k) * tIMU;
-rIMU(1) = rIMU(1) + rIMU(2) * tIMU;
+rIMUEst(2) = rIMUPrev(2) + measAccCorr(k) * tIMU;
+rIMUEst(1) = rIMUPrev(1) + rIMUPrev(2) * tIMU;
 % Sensor error update
-rIMU(3) = rIMU(3);
+rIMUEst(3) = rIMUPrev(3);
 % Delay error update
-rIMU(4) = rIMU(4);
+rIMUEst(4) = rIMUPrev(4);
 
 % Covariance prediction
 PEst = F*PEst*F' + Q;
