750ccc86d9661465decbcdeca3c1dfe057f56459
arnauochoa <arnau.ochoa@gmail.com>
2021-01-01 20:21:50 +0000

    standardEKF as inertial sensors subject

--- Estimation_Methods/standardEKF.m
+++ Estimation_Methods/standardEKF.m
@@ -20,23 +20,32 @@ if (~isnan(pGNSS)) % If GNSS position is available
 end
 
 % Initialization
-% F = [1 tIMU 0; 0 1 tIMU; 0 0 1];
-% F = [1 0 tIMU*cos(xOld(4)) 0 0 0;   ...   %%%% TODO: find correct F %%%%
-%      0 1 tIMU*sin(xOld(4)) 0 0 0;   ...
-%      0 0 1 0 0 0;                   ...
-%      0 0 0 1 0 0;                   ...
-%      0 0 0 0 1 0;                   ...
-%      0 0 0 0 0 1];
+% F = [1 tIMU 0; 0 1 tIMU; 0 0 1];  %%%% TODO: find correct F %%%%
+F       = eye(6);
+F(1, 3) = tIMU*cosd(xOld(4));               % d PNorth / d vAT
+F(1, 4) = -tIMU*xOld(3)*sind(xOld(4));      % d PNorth / d heading
+F(2, 3) = tIMU*sind(xOld(4));               % d PEast / d vAT
+F(2, 4) = tIMU*xOld(3)*cosd(xOld(4));       % d PEast / d heading
+F(3, 5) = tIMU;                             % d vAT / d biasAcc      %% Check this one: using dev for bias, not d bias
+F(4, 6) = tIMU;                             % d heading / d biasGyro %% Check this one: using dev for bias, not d bias
  
-Q = [0 0 0; 0 tIMU*Config.varAccNoise 0; 0 0 tIMU*Config.varAccBiasNoise];
+Q       = zeros(6);
+Q(3, 3) = Config.varAccNoise * tIMU;        % Along-Track Velocity
+Q(4, 4) = Config.varGyroNoise * tIMU;       % Heading
+Q(5, 5) = Config.varAccBiasNoise * tIMU;    % Accelerometer bias (error)
+Q(5, 5) = Config.varGyroBiasNoise * tIMU;   % Gyroscope bias (error)
     
 % Sensor error compensation
-measAccCorr = measAcc + xOld(3);
+measAccCorr = measAcc + xOld(5);
+measGyroCorr = measGyro + xOld(6);
 % Strapdown equations updated
-xEst(2) = xOld(2) + 0.5 * (measAccCorr + measAccCorrOld) * tIMU;
-xEst(1) = xOld(1) + 0.5 * (xEst(2) + xOld(2)) * tIMU;
-% Sensor error update
-xEst(3) = xOld(3);
+xEst(3) = xOld(3) + 0.5 * (measAccCorr + measAccCorrOld) * tIMU;
+xEst(4) = xOld(4) + 0.5 * (measGyroCorr + measGyroCorrOld) * tIMU;
+xEst(1) = xOld(1) + 0.5 * (xEst(3) + xOld(3)) * cosd(xOld(4)) * tIMU;
+xEst(2) = xOld(2) + 0.5 * (xEst(3) + xOld(3)) * sind(xOld(4)) * tIMU;      % TODO: check this, average heading?
+% % Sensor error update
+xEst(5) = xOld(5);
+xEst(6) = xOld(6);
 
 % Covariance prediction
 PEst = F*POld*F' + Q;
